feede6ecdb3225aa690bbab26b452c449040af0a
“Sergey <savershinin23@gmail.com>
2026-03-22 14:19:45 +0300

    Solving the task

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,31 @@
+import ShopItemFunc from './components/ShopItemFunc'
+import '../../components/store-func/img/item-black.jpg'
+import '../../components/store-func/img/item-blue.jpg'
+
+
+function App() {
+  const item = {
+    brand: 'Tiger of Sweden',
+    title: 'Leonard coat',
+    description: 'Minimalistic coat in cotton-blend',
+    descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
+    price: 399,
+    currency: '£'
+  }
+
+// Внутри компонента App
+return (
+  <div className="container">
+    <div className="background-element">
+    </div>
+    <div className="highlight-window">
+      <div className='highlight-overlay'></div>
+    </div>
+    <div className="window">
+      <ShopItemFunc item={item} />
+    </div>
+  </div>
+)
+}
+
+export default App
